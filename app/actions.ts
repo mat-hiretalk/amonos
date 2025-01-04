82786ad5d6714afd951c48b95170add80b52cd9f
mat-hiretalk <97069458+mat-hiretalk@users.noreply.github.com>
2025-01-05 07:55:28 +0800

    Update redirect paths in authentication actions to improve routing consistency
    - Changed redirect from "/pit" to "/protected" in signInAction and updateSession functions.
    - This update aligns the routing structure for user authentication and session management.

--- app/actions.ts
+++ app/actions.ts
@@ -53,7 +53,7 @@ export const signInAction = async (formData: FormData) => {
     return encodedRedirect("error", "/sign-in", error.message);
   }
 
-  return redirect("/pit");
+  return redirect("/protected");
 };
 
 export const forgotPasswordAction = async (formData: FormData) => {
